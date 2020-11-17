c352c1df708f0c9a635a80d7304987ebdbf2920f
Thulio Ferraz Assis <thulio.assis@suse.com>
2020-11-17 13:36:43 -0600

    bootstrap: api servicebroker/Offering
    Generated with:
    ```
    kubebuilder create api --group servicebroker --version v1alpha1 --kind Offering
    ```
    
    Signed-off-by: Thulio Ferraz Assis <thulio.assis@suse.com>

--- operator/main.go
+++ operator/main.go
@@ -74,6 +74,14 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "Provider")
 		os.Exit(1)
 	}
+	if err = (&controllers.OfferingReconciler{
+		Client: mgr.GetClient(),
+		Log:    ctrl.Log.WithName("controllers").WithName("Offering"),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Offering")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	setupLog.Info("starting manager")
