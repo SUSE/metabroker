bf061e7e42a705ac3239c65fed21695133aa7aa1
Thulio Ferraz Assis <thulio.assis@suse.com>
2020-11-17 13:38:01 -0600

    bootstrap: api servicebroker/Plan
    Generated with:
    ```
    kubebuilder create api --group servicebroker --version v1alpha1 --kind Plan
    ```
    
    Signed-off-by: Thulio Ferraz Assis <thulio.assis@suse.com>

--- operator/main.go
+++ operator/main.go
@@ -82,6 +82,14 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "Offering")
 		os.Exit(1)
 	}
+	if err = (&controllers.PlanReconciler{
+		Client: mgr.GetClient(),
+		Log:    ctrl.Log.WithName("controllers").WithName("Plan"),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Plan")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	setupLog.Info("starting manager")
