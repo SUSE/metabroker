b870a89138a8906bce6e60182b6af6bafbc5241c
Thulio Ferraz Assis <thulio.assis@suse.com>
2020-11-26 11:24:25 -0600

    bootstrap: api servicebroker/Credential
    Generated with
    kubebuilder create api --group servicebroker --version v1alpha1 --kind Credential --controller --resource
    
    Signed-off-by: Thulio Ferraz Assis <thulio.assis@suse.com>

--- operator/main.go
+++ operator/main.go
@@ -121,6 +121,14 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "Instance")
 		os.Exit(1)
 	}
+	if err = (&controllers.CredentialReconciler{
+		Client: mgr.GetClient(),
+		Log:    ctrl.Log.WithName("controllers").WithName("Credential"),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Credential")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	setupLog.Info("starting manager")
